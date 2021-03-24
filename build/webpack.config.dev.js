2fa9b6662030a1218670e8f04deff0a8c48ef098
xuyede <38210375+xuyede@users.noreply.github.com>
2021-03-24 10:10:34 +0800

    Update webpack.config.dev.js
    feat:add friendly-errors-webpack-plugin

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -3,29 +3,48 @@ const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 const HtmlPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+const devServerConfig = {
+    dev: {
+        https: false,
+        host: '127.0.0.1',
+        port: '12306'
+    }
+}
 
 const devServer = {
-    contentBase : path.resolve(__dirname, '../dist'),
-    compress : true,
-    progress : true,
-    hot : true
+    contentBase: path.resolve(__dirname, '../dist'),
+    overlay: true,
+    quiet: true,
+    port: devServerConfig.dev.port,
 }
 
 const devConfig = merge(baseConfig, {
-    mode : 'development',
+    mode: 'development',
     devServer,
-    plugins : [
+    plugins: [
         new HtmlPlugin({
-            template : path.resolve(__dirname, '../src/index.html'),
-            filename : 'index.html'
+            template: path.resolve(__dirname, '../src/index.html'),
+            filename: 'index.html'
         }),
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new FriendlyErrorsWebpackPlugin({
+            compilationSuccessInfo: {
+                messages: [
+                    `Your application is running here: ${
+                    devServerConfig.dev.https ? "https" : "http"
+                    }://${devServerConfig.dev.host}:${devServerConfig.dev.port}`,
+                ],
+            },
+            clearConsole: true,
+        }),
     ],
-    module : {
-        rules : [
+    module: {
+        rules: [
             {
-                test : /\.styl(us)?$/,
-                use : [
+                test: /\.styl(us)?$/,
+                use: [
                     'vue-style-loader',
                     'css-loader',
                     'postcss-loader',
